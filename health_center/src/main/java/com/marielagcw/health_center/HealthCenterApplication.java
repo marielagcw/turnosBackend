d2720d4ca3c7cb1b8bccb25432f46ca3d7a202e9
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2022-05-01 15:13:27 -0300

    v1.0.0 Project configuration

---
+++ health_center/src/main/java/com/marielagcw/health_center/HealthCenterApplication.java
@@ -0,0 +1,13 @@
+package com.marielagcw.health_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCenterApplication.class, args);
+	}
+
+}
